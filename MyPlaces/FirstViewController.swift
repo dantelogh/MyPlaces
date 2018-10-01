2c447d8c995467ade5de8a08cf089e0c897f038c
user143004 <user143004@87-237-62-4.rdns.melbourne.co.uk>
2018-10-01 09:40:17 -0700

    Initial Commit

---
+++ MyPlaces/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MyPlaces
+//
+//  Created by user143004 on 10/1/18.
+//  Copyright © 2018 user143004. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
