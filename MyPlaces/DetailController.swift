4421a15c4bd5c254019e3766210727a1ba1a442b
user143004 <user143004@87-237-62-4.rdns.melbourne.co.uk>
2018-10-03 08:30:58 -0700

    Esqueleto PLA 1

---
+++ MyPlaces/DetailController.swift
@@ -0,0 +1,33 @@
+//
+//  DetailController.swift
+//  MyPlaces
+//
+//  Created by user143004 on 10/1/18.
+//  Copyright © 2018 user143004. All rights reserved.
+//
+
+import UIKit
+
+class DetailController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func Close(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
