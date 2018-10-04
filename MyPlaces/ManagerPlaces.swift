aa17f64fde196cb96bbbb27b0a563c77e2e9282c
user143004 <user143004@87-237-62-4.rdns.melbourne.co.uk>
2018-10-04 09:27:17 -0700

    Clase "ManagerPlaces".

---
+++ MyPlaces/ManagerPlaces.swift
@@ -0,0 +1,47 @@
+//
+//  ManagerPlaces.swift
+//  MyPlaces
+//
+//  Created by user143004 on 10/4/18.
+//  Copyright © 2018 user143004. All rights reserved.
+//
+
+import Foundation
+
+class ManagerPlaces {
+    //Propiedades de la clase
+    var places: [Place] = []
+    
+    //Constructor de la clase
+    init() {
+        
+    }
+
+    //Métodos de la clase
+    func append(_ value: Place) {
+        
+    }
+    
+    func getCount() -> Int {
+        return places.count
+    }
+    
+    func getItemAt(position: Int) -> Place {
+        var place: Place
+        place = Place()
+
+        return place
+    }
+
+    func getItemById(id: String) -> Place {
+        var place: Place
+        place = Place()
+        
+        return place
+    }
+
+    func remove(_ value: Place) {
+        
+    }
+
+}
