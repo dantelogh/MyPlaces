1dd1e9455bb3dbd79f2c37ff585683a0dc913dd0
user143004 <user143004@87-237-62-4.rdns.melbourne.co.uk>
2018-10-04 23:33:50 -0700

    Modificar "FirstViewController"

--- MyPlaces/FirstViewController.swift
+++ MyPlaces/FirstViewController.swift
@@ -8,13 +8,42 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+//Se ha modificado el protocolo en la PLA 1
+//https://applecoding.com/analisis/programacion-orientada-protocolos-evolucion-swift-2
+//https://applecoding.com/cursos/swift-leccion-8-protocolos
+//class FirstViewController: UIViewController {
+class FirstViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    //--- Métodos implementados del protocolo ---
+    //Número de elementos del manager
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    //Para indicar subsecciones. En nuestro caso no tenemos (devolvemos un 1)
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    //Detectar pulsación de un elemento
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+    }
+
+    //Devolver la altura de la fila situada en un posición determinada
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        <#code#>
+    }
+    
+    //Devolver una instancia de la clase UITableViewCell que pinte la fila seleccionada
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
 
 }
 
